4d23b9bac1f71bb4e0cc783c26c9aa308face653
Kumilachew-g <kumilachew1212@gmail.com>
2023-01-06 11:59:49 +0300

    Utilize the actions for method

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,13 +1,23 @@
 class TodolistsController < ApplicationController
   def index
+    todolists = Todolist.order('created_at DESC')
+    render json: {status: 'SUCCESS', message: 'Loaded todolists', data: todolists}, status: :ok
   end
 
   def create
+    todolist = Todolist.create(todolist_params)
+    render json: {status: 'SUCCESS', message: 'Saved todolist', data: todolist}, status: :ok
   end
 
   def update
+    todolist = Todolist.find(params[:id])
+    todolist.update_attributes(todolist_params)
+    render json: {status: 'SUCCESS', message: 'Updated todolist', data: todolist}, status: :ok
   end
 
   def destroy
+    todolist = Todolist.find(params[:id])
+    todolist.destroy
+    render json: {status: 'SUCCESS', message: 'Deleted todolist', data: todolist}, status: :ok
   end
 end
