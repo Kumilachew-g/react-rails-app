e4c96ea6acc5a5d7df6cc840dbaf93539efa9430
Kumilachew-g <kumilachew1212@gmail.com>
2023-01-06 11:48:53 +0300

    Create Todolist controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'todolists/index'
+  get 'todolists/create'
+  get 'todolists/update'
+  get 'todolists/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
